80e3723caf7e77cf71c1e948ec987464dca234f4
edwin <edwin@kali.rival>
2026-08-30 18:41:28 -0400

    Clean up: remove node_modules and compiled files

--- assignment2/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const product25 = {
-    name: "CLB243 - Big Applique Voile Lace YFG - Yellow & Forest Green",
-    price: 490,
-    category: "Celebrant Laces",
-    url: "https://www.empiretextiles.com/products/laces/celebrant-laces/clb243-big-applique-voile-lace.html"
-};
-console.log(product25);
