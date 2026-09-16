ec1f27ec4cb8b0ed0d35e8ccef7a91b057276022
Rival_X <edwinfello50@gmail.com>
2026-09-16 17:11:51 -0400

    Delete My-first-app directory

--- My-first-app/src/app/myfirstreusablecomponent/myfirstreusablecomponent.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-myfirstreusablecomponent',
-  imports: [],
-  templateUrl: './myfirstreusablecomponent.html',
-  styleUrl: './myfirstreusablecomponent.css',
-})
-export class Myfirstreusablecomponent {}
--- My-first-app/src/app/pipe-ex/pipe-ex.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pipe-ex',
-  imports: [],
-  templateUrl: './pipe-ex.html',
-  styleUrl: './pipe-ex.css',
-})
-export class PipeEx {}
